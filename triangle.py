4ba4464756f48d5be74301a5c5f2597832827279
Ugoo <ugookoma(at)icloud.com>
2022-07-02 19:44:17 +0200

    Committed all

--- triangle.py
+++ triangle.py
@@ -1,3 +1,6 @@
+from math import *
+
+
 print("Hello world")
 
 print("    /|")
@@ -5,3 +8,26 @@ print("   / |")
 print("  /  |")
 print(" /   |")
 print("/____|")
+print(10 % 3)
+menu = 5, 6
+print(menu)
+print(str(menu))
+type(menu)
+mad = -5
+print(abs(mad))
+print(pow(10, 2))
+print(round(3.62))
+print(floor(4.4))
+print(ceil(5.1))
+print(sqrt(144))
+ele = sqrt(144)
+print(floor(ele))
+
+name = input("Enter a name: \n")
+age = input("Enter an age: \n")
+print("Hallo " + name + "!" + "Your age is " + age)
+
+num1 = int(input("Type here"))
+num2 = int(input("Type here"))
+result = num1 + num2
+print(result)
